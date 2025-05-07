b5dd9eb24a48064b43f4872133af8764139e3b0a
Jay <jp6631@srmist.edu.in>
2025-05-07 14:54:55 +0530

    Celebrity Face Recognition

---
+++ Server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask,request,jsonify
+
+app = Flask(__name__)
+
+@app.route('/classify image',methods = ['GET','POST'])
+def classify_image():
+    return "hi"
+
+if __name__=="__main__":
+    app.run(port=5000)
